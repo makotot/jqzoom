46a059be792d5656704931112f6308a773ffd911
makoto tateno <mtateno@yahoo.com>
2012-08-11 18:10:48 +0900

    test file add

---
+++ test/my-tests.js
@@ -0,0 +1,23 @@
+describe('jqzoom', function() {
+	var $elem = $('<a href="img/pig.jpg" class="jscZoom"><img src="../sample/img/pig_s.jpg" title="http://www.sxc.hu/photo/560043""></a>');
+
+	beforeEach(function() {
+		$elem.appendTo($('body'));
+	});
+	
+	describe('jquery', function() {
+		it('expect function', function() {
+			expect($).to.be.a('function');
+		});
+	});
+	describe('$(div.elem).zoom()', function() {
+		it('should be a elem', function() {
+			var $elem = $('<div class="jsiTest"></div>');
+			$elem.zoom().should.equal($elem);
+		});
+	});
+
+	afterEach(function() {
+		$elem.remove();
+	});
+});
